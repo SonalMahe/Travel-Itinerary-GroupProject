0d6927de190d0dc85b9872d6a8001cabb3ab5bc3
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-02-19 11:09:42 +0100

    changes in destination file

--- src/cli.ts
+++ src/cli.ts
@@ -1,17 +1,17 @@
-import { getIndiaInfo, getAllDestinations,} from "./services/destinationservices";
+import { getIndiaInfo, getAllDestinations,} from "./services/destinationservices.js";
 
-const india = await getIndiaInfo();
+const india = await getIndiaInfo("India");
 console.log(india.flag, india.countryName, india.currency);
 
 // Or get all 4 at once:
-const all = await getAllDestinations();
+const all = await getAllDestinations("India");
 // all.forEach((country: any) => console.log(country.countryName));
 all.forEach((country: any) =>
   console.log(
-    "\n",
-    country.flag,
-    country.countryName,
-    country.capital,
-    country.currency,
+     
+    "Flag:", country.flag,"\n",
+    "Country:", country.countryName,"\n",
+    "Capital:", country.capital,"\n",
+    "Currency:", country.currency,
   ),
 );
