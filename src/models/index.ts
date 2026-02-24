90bec7fcf9ad3843f108c4e48fbba5fac411d111
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-02-24 19:56:33 +0100

    updated

---
+++ src/models/index.ts
@@ -0,0 +1,25 @@
+
+// export type ActivityCategory =
+//   | "food"
+//   | "transport"
+//   | "sightseeing"
+//   | "accommodation";
+
+// export type Activity = {
+//   id: string;
+//   name: string;
+//   description?: string;
+//   cost: number;
+//   category: ActivityCategory;
+//   startTime: Date;
+//   endTime: Date;
+// };
+
+// export type Trip = {
+//   id: string;
+//   destination: string;
+//   currency: string;
+//   startDate: Date;
+//   endDate?: Date;
+//   activities: Activity[];
+// };
